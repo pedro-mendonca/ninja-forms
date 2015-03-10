816ba3ae75d9b797f112782bf045821f77494763
Kevin Stover <kstover@gmail.com>
2015-03-10 17:24:54 -0400

    Creating base field class.

---
+++ classes/field-base-type.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Base class for field types.
+ * This is the parent class. it should be extended by specific field types
+ *
+ * @package     Ninja Forms
+ * @subpackage  Classes/Fields
+ * @copyright   Copyright (c) 2015, WPNINJAS
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       3.0
+*/
+
+abstract class NF_Fields_Base_Type {
+	
+}
