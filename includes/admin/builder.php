92105f7a4744a5505814074289b54c6e94e776b3
James Laws <james@wpninjas.com>
2015-03-11 15:24:09 -0400

    First draft of new 3.0 UI structure

--- includes/admin/builder.php
+++ includes/admin/builder.php
@@ -21,5 +21,234 @@ function nf_register_tab_builder(){
 add_action( 'admin_init', 'nf_register_tab_builder' );
 
 function nf_tab_builder() {
-	
+	?>
+
+	<style>
+		.nf-form-builder {
+			padding: 20px 0;
+		}
+		.nf-field {
+			margin-bottom: 10px;
+			opacity: 0.7;
+		}
+		.nf-field:hover {
+			opacity: 1;
+		}
+		.nf-field.active {
+			opacity: 1;
+		}
+		.nf-field-header {
+			background: #fff;
+			border: 1px solid #ccc;
+			cursor: move;
+			margin: 0;
+			padding: 10px;
+		}
+		.nf-field-header .dashicons {
+			float: right;
+		}
+		.nf-field-body {
+			background: #fff;
+			border: 1px solid #ccc;
+			border-top: 0;
+			/*margin-top: -5px;*/
+			overflow: hidden;
+			padding-left: 200px;
+		}
+		.nf-field-body:after {
+			clear: both;
+			content: "";
+			display: block;
+		}
+		.nf-field.active .nf-field-header {
+
+		}
+		.nf-field-sidebar {
+			float: left;
+			width: 200px;
+			margin-left: -200px;
+		}
+		.nf-field-sidebar ul {
+			margin: 0;
+		}
+		.nf-field-sidebar li {
+			background: #f1f1f1;
+			border-right: 1px solid #ccc;
+			margin: 0;
+			padding: 8px 20px;
+		}
+		.nf-field-sidebar li:first-child {
+			background: #ccc;
+			padding: 8px;
+		}
+		.nf-field-sidebar li:first-child select {
+			width: 100%;
+		}
+		.nf-field-sidebar li:first-child label {
+			display: block;
+			padding: 0 8px 8px;
+		}
+		.nf-field-sidebar li.active {
+			background: #fff;
+			border: 1px solid #ccc;
+			border-right: 0;
+			border-left: 0;
+		}
+		.nf-field-content {
+			float: left;
+			width: 100%;
+		}
+		.nf-field-content .inside {
+			padding: 5px 20px;
+		}
+
+		.nf-form-builder label {
+			display: block;
+			/*vertical-align: middle;*/
+		}
+		.nf-form-builder .select label {
+			display: block;
+			padding-top: 8px;
+		}
+		.nf-form-builder .select .nf-desc {
+			display: block;
+		}
+		.nf-form-builder .select select {
+			margin-top: -3px;
+		}
+		dl.nf-field-settings {
+			display: block;
+			margin: 0 0 20px !important;
+			overflow: hidden;
+			padding-left: 18%;
+		}
+		.nf-field-settings dt {
+			float: left;
+			clear: both;
+			width: 21.951%;
+			padding: 0;
+			margin-left: -21.951%;
+			text-decoration: none;
+		}
+		.nf-field-settings dd {
+			float: left;
+			margin: 0;
+			width: 100%;
+		}
+		.nf-desc {
+			font-style: italic;
+		}
+
+		.nf-form-builder-bar {
+			background: #333;
+/*			border: 1px solid #fff;*/
+			padding: 10px;
+		}
+		.nf-field-selector a {
+			color: #424242;
+			text-decoration: none;
+		}
+		.nf-form-builder-bar .nf-item {
+			margin-left: 5px;
+		}
+		.nf-form-builder-bar .dashicons {
+			padding: 2px 8px 4px 0;
+		}
+		.nf-field-selector a.dashicons-arrow-down {
+			border-left: 1px solid #ccc;
+			padding: 2px 0 4px 5px;
+			margin-left: 8px;
+		}
+	</style>
+	<div class="nf-form-builder-bar">
+		<a href="#" class="button-primary nf-item">Save</a>
+		<div class="button-secondary nf-field-selector nf-item" style="float: right;"><a href="#">Add New Field</a><a class="dashicons dashicons-arrow-down"></a></div>
+		<a href="#" class="button-secondary nf-item" style="float: right;"><span class="dashicons dashicons-sort"></span>Expand Fields</a>
+
+	</div>
+	<div class="nf-form-builder">
+		<div class="nf-field">
+			<div class="nf-field-header">
+				First Name
+				<span class="dashicons dashicons-arrow-down"></span>
+			</div>
+		</div>
+		<div class="nf-field active">
+			<div class="nf-field-header">
+				Last Name
+				<span class="dashicons dashicons-arrow-up"></span>
+			</div>
+			<div class="nf-field-body">
+				<div class="nf-field-sidebar">
+					<ul class="nf-field-tabs">
+						<li>
+							<label>Select Field Type</label>
+							<select>
+								<option value="field-type">Single line Input</option>
+								<option value="field-type">Multi-Line Textarea</option>
+								<option value="field-type">Checkbox</option>
+								<option value="field-type">Checkbox List</option>
+								<option value="field-type">Dropdown List</option>
+								<option value="field-type">Radio List</option>
+							</select>
+						</li>
+						<li class="nf-field-tab">Basic</li>
+						<li class="nf-field-tab active">Restrictions</li>
+						<li class="nf-field-tab">Calculations</li>
+						<li class="nf-field-tab">Advanced</li>
+						<li class="nf-field-tab">Conditionals</li>
+						<li class="nf-field-tab">Styles</li>
+					</ul>
+				</div>
+				<div class="nf-field-content">
+					<div class="inside">
+						<dl class="nf-field-settings checkbox-input">
+							<dt>
+								<label for="nf-required">Required</label>
+							</dt>
+							<dd>
+								<input type="checkbox" id="nf-required" value="1">
+								<span class="nf-desc">This field must be completed for form to submit.</span>
+							</dd>
+						</dl>
+						<dl class="nf-field-settings select">
+							<dt>
+								<label for="nf-mask">Input Format</label>
+							</dt>
+							<dd>
+								<select id="nf-mask">
+									<option>None</option>
+									<option>Phone Number</option>
+									<option>Date</option>
+									<option selected="selected">Custom</option>
+								</select>
+								<input type="text" placeholder="999-99-9999" />
+								<span class="nf-desc">This will force user input into a specific format.</span>
+							</dd>
+						</dl>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="nf-field">
+			<div class="nf-field-header">
+				Email
+				<span class="dashicons dashicons-arrow-down"></span>
+			</div>
+		</div>
+		<div class="nf-field">
+			<div class="nf-field-header">
+				Message
+				<span class="dashicons dashicons-arrow-down"></span>
+			</div>
+		</div>
+		<div class="nf-field">
+			<div class="nf-field-header">
+				Submit
+				<span class="dashicons dashicons-arrow-down"></span>
+			</div>
+		</div>
+	</div>
+	<?
 }
