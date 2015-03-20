a0463ab29fcd6b7bdb6209771f383b96584c7705
Kyle B. Johnson <kyle@wpninjas.com>
2015-03-20 15:14:42 -0400

    add upgrade errors section

--- includes/admin/upgrades/convert-forms.php
+++ includes/admin/upgrades/convert-forms.php
@@ -15,7 +15,8 @@ class NF_Convert_Forms extends NF_Step_Processing {
 
         if( 0 == $wpdb->query( "SHOW TABLES LIKE '" . NINJA_FORMS_TABLE_NAME . "'" ) ) {
             $this->errors[] = NINJA_FORMS_TABLE_NAME . ' is not in the database';
-            $this->redirect = admin_url( 'admin.php?page=ninja-forms' );
+            //$this->redirect = admin_url( 'admin.php?page=ninja-forms' );
+            return false;
             return array( 'complete' => true );
         }
 
